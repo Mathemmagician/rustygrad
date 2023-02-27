4794cd5a3d3afef8e18b6bf9f729a074b8382d59
Mathemmagician <ramil9898@gmail.com>
2023-02-27 14:07:25 -0500

    micrograds api

--- src/main.rs
+++ src/main.rs
@@ -1,84 +1,110 @@
+#[macro_use]
+extern crate impl_ops;
+use std::ops;
 use std::{
     cell::RefCell,
-    fmt::{Debug, self},
-    ops::{Deref, Add, Mul, Neg},
+    fmt::{self, Debug},
+    ops::{Deref, Neg},
     rc::Rc,
 };
 
 #[macro_export]
 macro_rules! value {
-    ( $x:expr, $y:expr ) => {{
-        RValue(Rc::new(RefCell::new(Value::new($x, $y))))
+    ( $x:expr ) => {{
+        Value::from($x)
     }};
 }
 
 #[derive(Debug)]
-struct RValue(Rc<RefCell<Value>>);
+struct Value(Rc<RefCell<ValueData>>);
 
-impl Deref for RValue { 
-    type Target = Rc<RefCell<Value>>;
+impl Deref for Value {
+    type Target = Rc<RefCell<ValueData>>;
 
     fn deref(&self) -> &Self::Target {
         &self.0
     }
 }
 
-type BackwardsFn = fn(value: &Value);
+impl_op_ex!(+ |a: &Value, b: &Value| -> Value {
+    let out = value!(a.borrow().data + b.borrow().data);
+    out.borrow_mut()._prev = vec![Value(Rc::clone(&a)), Value(Rc::clone(&b))];
+    out.borrow_mut()._backward = Some(|value: &ValueData| {
+        value._prev[0].borrow_mut().grad += value.grad;
+        value._prev[1].borrow_mut().grad += value.grad;
+    });
+    out
+});
 
-struct Value {
+impl_op_ex!(-|a: &Value, b: &Value| -> Value { a + (-b) });
+
+impl_op_ex!(*|a: &Value, b: &Value| -> Value {
+    let out = value!(a.borrow().data * b.borrow().data);
+    out.borrow_mut()._prev = vec![Value(Rc::clone(&a)), Value(Rc::clone(&b))];
+    out.borrow_mut()._backward = Some(|value: &ValueData| {
+        value._prev[0].borrow_mut().grad += value._prev[1].borrow_mut().data * value.grad;
+        value._prev[1].borrow_mut().grad += value._prev[0].borrow_mut().data * value.grad;
+    });
+    out
+});
+
+impl_op_ex!(/ |a: &Value, b: &Value| -> Value {
+    a * b.pow(&value!(-1.0))
+});
+
+type BackwardsFn = fn(value: &ValueData);
+
+struct ValueData {
     data: f64,
     grad: f64,
-    label: String,
     _backward: Option<BackwardsFn>,
-    _prev: Vec<RValue>,
+    _prev: Vec<Value>,
 }
 
-
-impl Value {
-    fn new(data: f64, label: &str) -> Value {
-        Value {
+impl ValueData {
+    fn new(data: f64) -> ValueData {
+        ValueData {
             data,
             grad: 0.0,
-            label: label.to_string(),
             _backward: None,
             _prev: Vec::new(),
         }
     }
 }
 
-impl Debug for Value {
+impl Debug for ValueData {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         f.debug_struct("Value")
-         .field("data", &self.data)
-         .field("grad", &self.grad)
-         .field("label", &self.label)
-         .finish()
+            .field("data", &self.data)
+            .field("grad", &self.grad)
+            .finish()
     }
 }
 
-impl RValue {
-    fn new(value: Value) -> RValue {
-        RValue(Rc::new(RefCell::new(value)))
+impl<T: Into<f64>> From<T> for Value {
+    fn from(t: T) -> Value {
+        Value::new(ValueData::new(t.into()))
+    }
+}
+
+impl Value {
+    fn new(value: ValueData) -> Value {
+        Value(Rc::new(RefCell::new(value)))
     }
 
-    fn set_label(&self, label: &str) -> &RValue {
-        self.borrow_mut().label = label.to_string();
-        self
-    }
-
-    fn relu(&self) -> RValue {
-        let out = value!(self.borrow().data.max(0.0), "relu");
-        out.borrow_mut()._prev = vec![RValue(Rc::clone(&self))];
-        out.borrow_mut()._backward = Some(|value: &Value| {
+    fn relu(&self) -> Value {
+        let out = value!(self.borrow().data.max(0.0));
+        out.borrow_mut()._prev = vec![Value(Rc::clone(&self))];
+        out.borrow_mut()._backward = Some(|value: &ValueData| {
             value._prev[0].borrow_mut().grad += value.grad;
         });
         out
     }
 
-    fn pow(&self, other: &RValue) -> RValue {
-        let out = value!(self.borrow().data.powf(other.borrow().data), "**{other}");
-        out.borrow_mut()._prev = vec![RValue(Rc::clone(&self)), RValue(Rc::clone(&other))];
-        out.borrow_mut()._backward = Some(|value: &Value| {
+    fn pow(&self, other: &Value) -> Value {
+        let out = value!(self.borrow().data.powf(other.borrow().data));
+        out.borrow_mut()._prev = vec![Value(Rc::clone(&self)), Value(Rc::clone(&other))];
+        out.borrow_mut()._backward = Some(|value: &ValueData| {
             let base = value._prev[0].borrow().data;
             let p = value._prev[1].borrow().data;
             value._prev[0].borrow_mut().grad += p * base.powf(p - 1.0) * value.grad;
@@ -87,74 +113,54 @@ impl RValue {
     }
 }
 
+impl Neg for &Value {
+    type Output = Value;
 
-impl Add<&RValue> for &RValue {
-    type Output = RValue;
-
-    fn add(self, other: &RValue) -> RValue {
-        let out = value!(self.borrow().data + other.borrow().data, "+");
-        out.borrow_mut()._prev = vec![RValue(Rc::clone(&self)), RValue(Rc::clone(&other))];
-        out.borrow_mut()._backward = Some(|value: &Value| {
-            value._prev[0].borrow_mut().grad += value.grad;
-            value._prev[1].borrow_mut().grad += value.grad;
-        });
-        out
-    }
-}
-
-impl Mul<&RValue> for &RValue {
-    type Output = RValue;
-
-    fn mul(self, other: &RValue) -> RValue {
-        let out = value!(self.borrow().data * other.borrow().data, "*");
-        out.borrow_mut()._prev = vec![RValue(Rc::clone(&self)), RValue(Rc::clone(&other))];
-        out.borrow_mut()._backward = Some(|value: &Value| {
-            value._prev[0].borrow_mut().grad += value._prev[1].borrow_mut().data * value.grad;
-            value._prev[1].borrow_mut().grad += value._prev[0].borrow_mut().data * value.grad;
-        });
-        out
-    }
-}
-
-impl Neg for &RValue {
-    type Output = RValue;
-
-    fn neg(self) -> RValue {
-        let neg = value!(-1.0, "");
+    fn neg(self) -> Value {
+        let neg = value!(-1.0);
         self * &neg
     }
 }
 
-
 fn main() {
+    // Micrograd:
+    // a = Value(-4.0)
+    let a = value!(-4.0);
+    // b = Value(2.0)
+    let b = value!(2.0);
+    // c = a + b
+    let mut c = &a + &b;
+    // d = a * b + b**3
+    let mut d = &a * &b + &b.pow(&value!(3.0));
+    // c += c + 1
+    c = &c + &c + value!(1.0);
+    // c += 1 + c + (-a)
+    c = &c + value!(1.0) + &c + (-&a);
+    // d += d * 2 + (b + a).relu()
+    d = &d + &d * value!(2.0) + (&b + &a).relu();
+    // d += 3 * d + (b - a).relu()
+    d = &d + value!(3.0) * &d + (&b - &a).relu();
+    // e = c - d
+    let e = &c - &d;
+    // f = e**2
+    let f = e.pow(&value!(2.0));
+    // g = f / 2.0
+    let mut g = &f / &value!(2.0);
+    // g += 10.0 / f
+    g = &g + &value!(10.0) / &f;
 
-    let a = value!(-4.0, "a");
-    let b = value!(2.0, "b");
-    let c = &a + &b;
-    c.set_label("a + b");
-    let d = &(&a * &b) + &b.pow(&value!(3.0, ""));
-    d.set_label("a * c");
-    let e = -&d;
-    e.set_label(" - d");
-    let f = a.pow(&value!(-1.0, ""));
-    f.set_label("a ** -1");
-    // let e = d.tanh();
+    // print(f'{g.data:.4f}') # prints 24.7041, the outcome of this forward pass
+    println!("{:.4}", g.borrow().data); // 24.7041
 
-    // e.set_grad(-1.0);
+    // g.backward()
+    // print(f'{a.grad:.4f}') # prints 138.8338, i.e. the numerical value of dg/da
+    // print(f'{b.grad:.4f}') # prints 645.5773, i.e. the numerical value of dg/db
 
-    println!("a is {:?}", a);
-    println!("b is {:?}", b);
-    println!("c is {:?}", c);
-    println!("d is {:?}", d);
-    println!("e is {:?}", e);
-    println!("f is {:?}", f);
-
-    // propagate_gradient(&e);
-
-    // dbg!(&x);
     // println!("a is {:?}", a);
     // println!("b is {:?}", b);
     // println!("c is {:?}", c);
     // println!("d is {:?}", d);
     // println!("e is {:?}", e);
+    // println!("f is {:?}", f);
+    // println!("g is {:?}", g);
 }
