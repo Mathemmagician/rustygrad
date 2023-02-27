703c2813d042fe9876bb3f2cc5568103ad945bf1
Mathemmagician <ramil9898@gmail.com>
2023-02-27 11:48:13 -0500

    great simplification

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::{
     cell::RefCell,
-    fmt::Debug,
-    ops::{Add, Mul},
+    fmt::{Debug, self},
+    ops::{Deref, Add, Mul, Neg},
     rc::Rc,
 };
 
@@ -12,33 +12,47 @@ macro_rules! value {
     }};
 }
 
-#[derive(Debug, PartialEq, Eq)]
-enum Operation {
-    Add,
-    Mul,
-    Tanh,
-    Relu
+#[derive(Debug)]
+struct RValue(Rc<RefCell<Value>>);
+
+impl Deref for RValue { 
+    type Target = Rc<RefCell<Value>>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
 }
 
-#[derive(Eq, PartialEq, Debug)]
-struct RValue(Rc<RefCell<Value>>);
+type BackwardsFn = fn(value: &Value);
 
 struct Value {
     data: f64,
-    label: String,
     grad: f64,
-    operation: Option<Operation>,
-    children: Vec<RValue>,
+    label: String,
+    _backward: Option<BackwardsFn>,
+    _prev: Vec<RValue>,
+}
+
+
+impl Value {
+    fn new(data: f64, label: &str) -> Value {
+        Value {
+            data,
+            grad: 0.0,
+            label: label.to_string(),
+            _backward: None,
+            _prev: Vec::new(),
+        }
+    }
 }
 
 impl Debug for Value {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         f.debug_struct("Value")
-            .field("label", &self.label)
-            .field("data", &self.data)
-            .field("grad", &self.grad)
-            .field("operation", &self.operation)
-            .finish()
+         .field("data", &self.data)
+         .field("grad", &self.grad)
+         .field("label", &self.label)
+         .finish()
     }
 }
 
@@ -48,83 +62,43 @@ impl RValue {
     }
 
     fn set_label(&self, label: &str) -> &RValue {
-        self.0.borrow_mut().label = label.to_string();
+        self.borrow_mut().label = label.to_string();
         self
-    }
-
-    fn set_grad(&self, grad: f64) -> &RValue {
-        self.0.borrow_mut().grad = grad;
-        self
-    }
-
-    fn add_grad(&self, grad: f64) -> &RValue {
-        self.0.borrow_mut().grad += grad;
-        self
-    }
-
-    fn tanh(&self) -> RValue {
-        let c = (2.0 * self.0.borrow().data).exp();
-        let data = (c - 1.0) / (c + 1.0);
-
-        RValue::new(Value {
-            data,
-            label: String::from("tanh"),
-            grad: 0.0,
-            operation: Some(Operation::Tanh),
-            children: vec![RValue(Rc::clone(&self.0))],
-        })
     }
 
     fn relu(&self) -> RValue {
-        let data = self.0.borrow().data.max(0.0);
+        let out = value!(self.borrow().data.max(0.0), "relu");
+        out.borrow_mut()._prev = vec![RValue(Rc::clone(&self))];
+        out.borrow_mut()._backward = Some(|value: &Value| {
+            value._prev[0].borrow_mut().grad += value.grad;
+        });
+        out
+    }
 
-        RValue::new(Value {
-            data,
-            label: String::from("tanh"),
-            grad: 0.0,
-            operation: Some(Operation::Tanh),
-            children: vec![RValue(Rc::clone(&self.0))],
-        })
+    fn pow(&self, other: &RValue) -> RValue {
+        let out = value!(self.borrow().data.powf(other.borrow().data), "**{other}");
+        out.borrow_mut()._prev = vec![RValue(Rc::clone(&self)), RValue(Rc::clone(&other))];
+        out.borrow_mut()._backward = Some(|value: &Value| {
+            let base = value._prev[0].borrow().data;
+            let p = value._prev[1].borrow().data;
+            value._prev[0].borrow_mut().grad += p * base.powf(p - 1.0) * value.grad;
+        });
+        out
     }
 }
 
-impl Value {
-    fn new(data: f64, label: &str) -> Value {
-        Value {
-            data,
-            label: label.to_string(),
-            grad: 0.0,
-            operation: None,
-            children: Vec::new(),
-        }
-    }
-}
-
-impl PartialEq for Value {
-    fn eq(&self, other: &Self) -> bool {
-        self.data == other.data
-            && self.grad == other.grad
-            && self.label == other.label
-            && self.operation == other.operation
-            && self.children == other.children
-    }
-}
-
-impl Eq for Value {}
 
 impl Add<&RValue> for &RValue {
     type Output = RValue;
 
     fn add(self, other: &RValue) -> RValue {
-        let data = self.0.borrow().data + other.0.borrow().data;
-
-        RValue::new(Value {
-            data,
-            label: String::from("+"),
-            grad: 0.0,
-            operation: Some(Operation::Add),
-            children: vec![RValue(Rc::clone(&self.0)), RValue(Rc::clone(&other.0))],
-        })
+        let out = value!(self.borrow().data + other.borrow().data, "+");
+        out.borrow_mut()._prev = vec![RValue(Rc::clone(&self)), RValue(Rc::clone(&other))];
+        out.borrow_mut()._backward = Some(|value: &Value| {
+            value._prev[0].borrow_mut().grad += value.grad;
+            value._prev[1].borrow_mut().grad += value.grad;
+        });
+        out
     }
 }
 
@@ -132,80 +106,55 @@ impl Mul<&RValue> for &RValue {
     type Output = RValue;
 
     fn mul(self, other: &RValue) -> RValue {
-        let data = self.0.borrow().data * other.0.borrow().data;
-
-        RValue::new(Value {
-            data,
-            label: String::from("*"),
-            grad: 0.0,
-            operation: Some(Operation::Mul),
-            children: vec![RValue(Rc::clone(&self.0)), RValue(Rc::clone(&other.0))],
-        })
+        let out = value!(self.borrow().data * other.borrow().data, "*");
+        out.borrow_mut()._prev = vec![RValue(Rc::clone(&self)), RValue(Rc::clone(&other))];
+        out.borrow_mut()._backward = Some(|value: &Value| {
+            value._prev[0].borrow_mut().grad += value._prev[1].borrow_mut().data * value.grad;
+            value._prev[1].borrow_mut().grad += value._prev[0].borrow_mut().data * value.grad;
+        });
+        out
     }
 }
 
-fn propagate_gradient(value: &RValue) {
-    let operation = &value.0.borrow().operation;
-    let data = value.0.borrow().data;
-    let grad = value.0.borrow().grad;
+impl Neg for &RValue {
+    type Output = RValue;
 
-    if let Some(opp) = operation {
-        // println!("{:?}", opp);
-        let children = &value.0.borrow().children;
-
-        if let Some(left) = &children.get(0) {
-            if let Some(right) = &children.get(1) {
-                match opp {
-                    Operation::Add => {
-                        left.add_grad(grad);
-                        right.add_grad(grad);
-                    }
-                    Operation::Mul => {
-                        left.add_grad(grad * right.0.borrow().data);
-                        right.add_grad(grad * left.0.borrow().data);
-                    }
-                    _ => (),
-                };
-                propagate_gradient(right);
-            } else {
-                match opp {
-                    Operation::Tanh => {
-                        let c = (2.0 * data).exp();
-                        let t = (c - 1.0) / (c + 1.0);
-                        left.add_grad((1.0 - t.powi(2)) * grad);
-                    }
-                    _ => (),
-                }
-            }
-            propagate_gradient(left);
-        }
+    fn neg(self) -> RValue {
+        let neg = value!(-1.0, "");
+        self * &neg
     }
 }
+
 
 fn main() {
-    // let a = RValue::new(Value::new(2.0, "a"));
-    let a = value!(2.0, "a");
-    let b = value!(-3.0, "b");
-    let c = &a + &b;
-    c.set_label("c");
-    let d = &a * &c;
-    d.set_label("d");
-    let e = d.tanh();
 
-    e.set_grad(-1.0);
+    let a = value!(-4.0, "a");
+    let b = value!(2.0, "b");
+    let c = &a + &b;
+    c.set_label("a + b");
+    let d = &(&a * &b) + &b.pow(&value!(3.0, ""));
+    d.set_label("a * c");
+    let e = -&d;
+    e.set_label(" - d");
+    let f = a.pow(&value!(-1.0, ""));
+    f.set_label("a ** -1");
+    // let e = d.tanh();
+
+    // e.set_grad(-1.0);
 
     println!("a is {:?}", a);
     println!("b is {:?}", b);
     println!("c is {:?}", c);
     println!("d is {:?}", d);
     println!("e is {:?}", e);
+    println!("f is {:?}", f);
 
-    propagate_gradient(&e);
+    // propagate_gradient(&e);
 
     // dbg!(&x);
-    println!("a is {:?}", a);
-    println!("b is {:?}", b);
-    println!("c is {:?}", c);
-    println!("d is {:?}", d);
-    println!("e is {:?}", e);
+    // println!("a is {:?}", a);
+    // println!("b is {:?}", b);
+    // println!("c is {:?}", c);
+    // println!("d is {:?}", d);
+    // println!("e is {:?}", e);
 }
