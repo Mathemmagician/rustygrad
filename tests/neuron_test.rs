a913e67f55e9f3839eb2e50d61d8eeba6ce98f5d
Mathemmagician <ramil9898@gmail.com>
2023-03-05 21:47:51 -0500

    fix and improve to neuron test

--- tests/neuron_test.rs
+++ tests/neuron_test.rs
@@ -3,11 +3,15 @@ use micrograd::Value;
 
 #[test]
 fn neuron_example() {
-    let x = vec![Value::from(1.0), Value::from(-2.0)];
-    let n = Neuron::from(2);
-    let ws: Vec<f64> = n.parameters().iter().map(|w| w.borrow().data).collect();
+    for _ in 1..20 {
+        let x = vec![Value::from(1.0), Value::from(-2.0)];
+        let n = Neuron::from(2);
+        let ws: Vec<f64> = n.parameters().iter().map(|w| w.borrow().data).collect();
 
-    let z = n.forward(&x);
-
-    assert_eq!(z.borrow().data, ws[0] + ws[1] * 1.0 + ws[2] * (-2.0));
+        let z = n.forward(&x);
+        assert_eq!(
+            z.borrow().data,
+            (ws[0] + ws[1] * 1.0 + ws[2] * (-2.0)).max(0.0)
+        );
+    }
 }
