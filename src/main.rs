cfad9d976ca4e548c38de2321b498676104d82f8
Mathemmagician <ramil9898@gmail.com>
2023-03-06 10:30:43 -0500

    minor

--- src/main.rs
+++ src/main.rs
@@ -11,87 +11,6 @@ fn main() {
     mlp();
 }
 
-fn loss(model: &MLP, xs: &[Vec<f64>], ys: &[f64]) -> (Value, f64) {
-    let inputs: Vec<Vec<Value>> = xs
-        .iter()
-        .map(|xrow| vec![Value::from(xrow[0]), Value::from(xrow[1])])
-        .collect();
-
-    // forward the model to get scores
-    let scores: Vec<Value> = inputs
-        .iter()
-        .map(|xrow| model.forward(xrow.clone())[0].clone())
-        .collect();
-
-    // svm "max-margin" loss
-    let losses: Vec<Value> = ys
-        .iter()
-        .zip(&scores)
-        .map(|(yi, scorei)| (1.0 + -yi * scorei).relu())
-        .collect();
-    let n: f64 = (&losses).len() as f64;
-    let data_loss: Value = losses.into_iter().sum::<Value>() / n;
-
-    // L2 regularization
-    let alpha: f64 = 0.0001;
-    let reg_loss: Value = alpha
-        * model
-            .parameters()
-            .iter()
-            .map(|p| p * p)
-            .into_iter()
-            .sum::<Value>();
-    let total_loss = data_loss + reg_loss;
-
-    // also get accuracy
-    let accuracies: Vec<bool> = ys
-        .iter()
-        .zip(scores.iter())
-        .map(|(yi, scorei)| (*yi > 0.0) == (scorei.borrow().data > 0.0))
-        .collect();
-    let accuracy = accuracies.iter().filter(|&a| *a).count() as f64 / n;
-
-    (total_loss, accuracy)
-}
-
-fn mlp() {
-    let model = MLP::new(2, vec![16, 16, 1]);
-
-    let (xs, ys) = load_moons_data();
-
-    // optimization
-    for k in 0..100 {
-        // forward
-        let (total_loss, acc) = loss(&model, &xs, &ys);
-
-        // backward
-        model.zero_grad();
-        total_loss.backward();
-
-        // update (sgd)
-        let learning_rate = 1.0 - 0.9 * (k as f64) / 100.0;
-        for p in &model.parameters() {
-            let delta = learning_rate * p.borrow().grad;
-            p.borrow_mut().data -= delta;
-        }
-
-        println!(
-            "step {k} loss {:.3}, accuracy {:.2}%",
-            total_loss.borrow().data,
-            acc * 100.0
-        );
-    }
-}
-
-fn nn() {
-    let n = Neuron::from(2);
-    let x = vec![Value::from(1.0), Value::from(-2.0)];
-
-    println!("n = {:?}", n);
-    let y = n.forward(&x);
-    println!("{:?}", y);
-}
-
 fn value() {
     // a = Value(-4.0)
     // b = Value(2.0)
@@ -138,4 +57,85 @@ fn value() {
     println!("e is {:?}", e);
     println!("f is {:?}", f);
     println!("g is {:?}", g);
+}
+
+fn nn() {
+    let n = Neuron::from(2);
+    let x = vec![Value::from(1.0), Value::from(-2.0)];
+
+    println!("n = {:?}", n);
+    let y = n.forward(&x);
+    println!("{:?}", y);
+}
+
+fn mlp() {
+    let model = MLP::new(2, vec![16, 16, 1]);
+
+    let (xs, ys) = load_moons_data();
+
+    // optimization
+    for k in 0..100 {
+        // forward
+        let (total_loss, acc) = loss(&model, &xs, &ys);
+
+        // backward
+        model.zero_grad();
+        total_loss.backward();
+
+        // update (sgd)
+        let learning_rate = 1.0 - 0.9 * (k as f64) / 100.0;
+        for p in &model.parameters() {
+            let delta = learning_rate * p.borrow().grad;
+            p.borrow_mut().data -= delta;
+        }
+
+        println!(
+            "step {k} loss {:.3}, accuracy {:.2}%",
+            total_loss.borrow().data,
+            acc * 100.0
+        );
+    }
+}
+
+fn loss(model: &MLP, xs: &[Vec<f64>], ys: &[f64]) -> (Value, f64) {
+    let inputs: Vec<Vec<Value>> = xs
+        .iter()
+        .map(|xrow| vec![Value::from(xrow[0]), Value::from(xrow[1])])
+        .collect();
+
+    // forward the model to get scores
+    let scores: Vec<Value> = inputs
+        .iter()
+        .map(|xrow| model.forward(xrow.clone())[0].clone())
+        .collect();
+
+    // svm "max-margin" loss
+    let losses: Vec<Value> = ys
+        .iter()
+        .zip(&scores)
+        .map(|(yi, scorei)| (1.0 + -yi * scorei).relu())
+        .collect();
+    let n: f64 = (&losses).len() as f64;
+    let data_loss: Value = losses.into_iter().sum::<Value>() / n;
+
+    // L2 regularization
+    let alpha: f64 = 0.0001;
+    let reg_loss: Value = alpha
+        * model
+            .parameters()
+            .iter()
+            .map(|p| p * p)
+            .into_iter()
+            .sum::<Value>();
+    let total_loss = data_loss + reg_loss;
+
+    // also get accuracy
+    let accuracies: Vec<bool> = ys
+        .iter()
+        .zip(scores.iter())
+        .map(|(yi, scorei)| (*yi > 0.0) == (scorei.borrow().data > 0.0))
+        .collect();
+    let accuracy = accuracies.iter().filter(|&a| *a).count() as f64 / n;
+
+    (total_loss, accuracy)
 }
